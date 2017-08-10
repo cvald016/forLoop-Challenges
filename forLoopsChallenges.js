5e08a0dd2993a0d7a8e57ef8de24d19764ba5eb4
Christopher <cvald016@fiu.edu>
2017-08-09 22:58:07 -0400

    forLoop challenges

---
+++ forLoopsChallenges.js
@@ -0,0 +1,54 @@
+//Reverse the order of an array
+
+// printReverse([1,2,3,4])
+
+// function printReverse(arg){
+//   for(var i = arg.length - 1; i >= 0; i--){
+//     console.log(arg[i]);
+//   }
+// }
+
+// printReverse(["?", "you", "are", "how"]);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+//Check to see if all items in array are uniform
+
+// function isUniform(arg){
+//   var first = arg[0];
+//   for (var i = 0; i < arg.length; i++ ){
+//     if (arg[i] !== first){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// isUniform([1, 1, 1, 1, 1, 1]);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+//Get the sum of all numbers in an array
+
+// function sumArray(arg){
+//   var sum = 0;
+//   arg.forEach(function(num){
+//     sum += num;
+//   });
+//   return sum;
+// }
+
+// sumArray([1, 2, 3, 4]);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+//Find the largest number in an array
+
+// function max(arg){
+//   var max = arg[0];
+//   arg.forEach(function(num){
+//     if(num > max){
+//       max = num;
+//     }
+//   });
+//   return max;
+// }
+
+// max([1,2,3,4,15,6]);
